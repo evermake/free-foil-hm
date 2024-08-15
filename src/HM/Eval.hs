bc5561c6f80ace31c616439474b3538de78078f0
Vladislav Deryabkin <53311479+evermake@users.noreply.github.com>
2024-08-15 08:36:32 +0300

    implement evaluation (#1)
    Co-authored-by: frog-da <84839431+frog-da@users.noreply.github.com>

--- src/HM/Eval.hs
+++ src/HM/Eval.hs
@@ -2,8 +2,24 @@ module HM.Eval where
 
 import HM.Parser.Abs
 
-interpret :: Exp -> String
-interpret ETrue = "true"
-interpret EFalse = "false"
-interpret (ENat nat) = show nat
-interpret (EAdd l r) = "(" ++ interpret l ++ " + " ++ interpret r ++ ")"
+interpret :: Exp -> Either String Exp
+interpret ETrue = Right ETrue
+interpret EFalse = Right EFalse
+interpret (ENat n) = Right (ENat n)
+interpret (EAdd l r) =
+  case (interpret l, interpret r) of
+    (Right (ENat l'), Right (ENat r')) -> Right (ENat (l' + r'))
+    _ -> Left "Unsupported expression in addition"
+interpret (ESub l r) =
+  case (interpret l, interpret r) of
+    (Right (ENat l'), Right (ENat r')) -> Right (ENat (l' - r'))
+    _ -> Left "Unsupported expression in subtraction"
+interpret (EIf cond then_ else_) =
+  case interpret cond of
+    Right ETrue -> interpret then_
+    Right EFalse -> interpret else_
+    _ -> Left "Unsupported condition in if statement"
+interpret (EIsZero n) =
+  case interpret n of
+    Right (ENat n') -> if n' == 0 then Right ETrue else Right EFalse
+    _ -> Left "Unsupported expression in iszero"
