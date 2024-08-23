798f1125f3cde1a5b625b54d36d2a064973bbed0
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-08-23 18:12:14 +0300

    Add unit tests

---
+++ test/HM/InterpretSpec.hs
@@ -0,0 +1,32 @@
+module HM.InterpretSpec where
+
+import           Test.Hspec
+import Control.Monad (forM_)
+import           HM.Interpret
+
+isSuccess :: Result -> Bool
+isSuccess Success{} = True
+isSuccess _ = False
+
+isTypeError :: Result -> Bool
+isTypeError (Failure TypecheckingError _) = True
+isTypeError _ = False
+
+wellTypedPaths :: [FilePath]
+wellTypedPaths = ["well-typed/1.lam", "well-typed/2.lam"]
+
+illTypedPaths :: [FilePath]
+illTypedPaths = ["ill-typed/1.lam", "ill-typed/2.lam"]
+
+spec :: Spec
+spec = do
+
+  describe "well-typed expressions" $ do
+    forM_ wellTypedPaths $ \path -> it path $ do
+      contents <- readFile ("test/files/" ++ path)
+      interpret contents `shouldSatisfy` isSuccess
+
+  describe "ill-typed expressions" $ do
+    forM_ illTypedPaths $ \path -> it path $ do
+      contents <- readFile ("test/files/" ++ path)
+      interpret contents `shouldSatisfy` isTypeError
